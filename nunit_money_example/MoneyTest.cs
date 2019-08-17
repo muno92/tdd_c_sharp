cbf1474303e6954fdf09ee420848dd685fd39e6d
n-ogawa <fy59.kou-729@live.jp>
2019-08-17 17:48:03 +0900

    Times()がDollarを返す事がわかりやすくなるようにアサーションを変更

--- nunit_money_example/MoneyTest.cs
+++ nunit_money_example/MoneyTest.cs
@@ -9,10 +9,8 @@ namespace nunit_money_example
         public void TestMultiplication()
         {
             Dollar five = new Dollar(5);
-            Dollar product = five.Times(2);
-            Assert.AreEqual(10, product.amount);
-            product = five.Times(3);
-            Assert.AreEqual(15, product.amount);
+            Assert.AreEqual(new Dollar(10), five.Times(2));
+            Assert.AreEqual(new Dollar(15), five.Times(3));
         }
 
         [Test]
