9ca325dca8310045e713987cad2108ad625403c4
n-ogawa <fy59.kou-729@live.jp>
2019-08-18 14:22:28 +0900

    テストコード内でDollarインスタンスを生成している箇所をFactory Methodに変更

--- nunit_money_example/MoneyTest.cs
+++ nunit_money_example/MoneyTest.cs
@@ -9,18 +9,18 @@ namespace nunit_money_example
         public void TestMultiplication()
         {
             Money five = Money.Dollar(5);
-            Assert.AreEqual(new Dollar(10), five.Times(2));
-            Assert.AreEqual(new Dollar(15), five.Times(3));
+            Assert.AreEqual(Money.Dollar(10), five.Times(2));
+            Assert.AreEqual(Money.Dollar(15), five.Times(3));
         }
 
         [Test]
         public void TestEquality()
         {
-            Assert.True(new Dollar(5).Equals(new Dollar(5)));
-            Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(Money.Dollar(5).Equals(Money.Dollar(5)));
+            Assert.False(Money.Dollar(5).Equals(Money.Dollar(6)));
             Assert.True(new Franc(5).Equals(new Franc(5)));
             Assert.False(new Franc(5).Equals(new Franc(6)));
-            Assert.False(new Franc(5).Equals(new Dollar(5)));
+            Assert.False(new Franc(5).Equals(Money.Dollar(5)));
         }
         
         [Test]
