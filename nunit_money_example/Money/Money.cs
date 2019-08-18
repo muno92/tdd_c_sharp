b6e9be94bec23945c2c40d523f6d9a05cb2fea79
n-ogawa <fy59.kou-729@live.jp>
2019-08-18 11:33:08 +0900

    Equalsでオブジェクトの比較もするよう調整

--- nunit_money_example/Money/Money.cs
+++ nunit_money_example/Money/Money.cs
@@ -7,7 +7,7 @@ namespace nunit_money_example.Money
         public override bool Equals(object obj)
         {
             Money money = (Money) obj;
-            return Amount == money.Amount;
+            return Amount == money.Amount && GetType() == money.GetType();
         }
     }
 }
