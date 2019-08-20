47bf01603f8f97b0de2f40375a177eeb27c1c102
n-ogawa <fy59.kou-729@live.jp>
2019-08-20 22:42:02 +0900

    Money.Equalsでクラスではなく通貨を比較するよう変更

--- nunit_money_example/Money/Money.cs
+++ nunit_money_example/Money/Money.cs
@@ -19,7 +19,7 @@ namespace nunit_money_example.money
         public override bool Equals(object obj)
         {
             Money money = (Money) obj;
-            return Amount == money.Amount && GetType() == money.GetType();
+            return Amount == money.Amount && Currency == money.Currency;
         }
 
         public override string ToString()
