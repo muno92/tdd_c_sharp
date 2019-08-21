c4b31800ae46a3b890ccff18baad1899582bfe67
n-ogawa <fy59.kou-729@live.jp>
2019-08-21 22:46:12 +0900

    未使用になったDollarクラスを削除

--- nunit_money_example/Money/Dollar.cs
+++
@@ -1,9 +0,0 @@
-namespace nunit_money_example.money
-{
-    public class Dollar : Money
-    {
-        public Dollar(int amount, string currency) : base(amount, currency)
-        {
-        }
-    }
-}
