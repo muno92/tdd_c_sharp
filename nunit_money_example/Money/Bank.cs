a1578f138a50b894caf32563e434a6d1ea3e180c
n-ogawa <fy59.kou-729@live.jp>
2019-08-21 23:53:30 +0900

    ReduceにMoneyが渡された場合の処理を追加

--- nunit_money_example/Money/Bank.cs
+++ nunit_money_example/Money/Bank.cs
@@ -4,6 +4,10 @@ namespace nunit_money_example.money
     {
         public Money Reduce(Expression source, string to)
         {
+            if (source.GetType() == typeof(Money))
+            {
+                return (Money) source;
+            }
             Sum sum = (Sum) source;
             return sum.Reduce(to);
         }
