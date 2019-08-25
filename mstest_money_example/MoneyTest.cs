203242dd4fc62ea7e680a94772b16de1f74beb62
n-ogawa <fy59.kou-729@live.jp>
2019-08-25 17:34:37 +0900

    フランを足し合わせるテストコードを作成

--- mstest_money_example/MoneyTest.cs
+++ mstest_money_example/MoneyTest.cs
@@ -20,5 +20,13 @@ namespace mstest_money_example
             Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
         }
+        
+        [TestMethod]
+        public void TestFrancMultiplication()
+        {
+            Franc five = new Franc(5);
+            Assert.AreEqual(new Franc(10), five.Times(2));
+            Assert.AreEqual(new Franc(15), five.Times(3));
+        }
     }
 }
