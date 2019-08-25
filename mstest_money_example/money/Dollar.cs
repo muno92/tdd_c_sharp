c9b02ecebd6b6a9b3fc99ed0e40f4753df8a638c
n-ogawa <fy59.kou-729@live.jp>
2019-08-25 12:40:55 +0900

    テストコードが動く状態に調整

---
+++ mstest_money_example/money/Dollar.cs
@@ -0,0 +1,15 @@
+namespace mstest_money_example.money
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+        }
+
+        public int Amount { get; set; }
+
+        public void Times(int multiplier)
+        {
+        }
+    }
+}
