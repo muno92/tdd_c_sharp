e3066f6be8c17f77f43efcfefe7ddda7f87b7374
n-ogawa <fy59.kou-729@live.jp>
2019-08-25 18:05:39 +0900

    テストコード内でDollarインスタンスを直接生成している箇所を全てDollarメソッドを利用するよう変更

--- mstest_money_example/MoneyTest.cs
+++ mstest_money_example/MoneyTest.cs
@@ -10,18 +10,18 @@ namespace mstest_money_example
         public void TestMultiplication()
         {
             Money five = Money.Dollar(5);
-            Assert.AreEqual(new Dollar(10), five.Times(2));
-            Assert.AreEqual(new Dollar(15), five.Times(3));
+            Assert.AreEqual(Money.Dollar(10), five.Times(2));
+            Assert.AreEqual(Money.Dollar(15), five.Times(3));
         }
         
         [TestMethod]
         public void TestEquality()
         {
-            Assert.IsTrue(new Dollar(5).Equals(new Dollar(5)));
-            Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
+            Assert.IsTrue(Money.Dollar(5).Equals(Money.Dollar(5)));
+            Assert.IsFalse(Money.Dollar(5).Equals(Money.Dollar(6)));
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
-            Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
+            Assert.IsFalse(new Franc(5).Equals(Money.Dollar(5)));
         }
         
         [TestMethod]
