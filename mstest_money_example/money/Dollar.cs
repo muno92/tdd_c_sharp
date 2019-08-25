93ca323ac4bf6106d4dbbda58aaf4bee063c60e8
n-ogawa <fy59.kou-729@live.jp>
2019-08-25 12:47:58 +0900

    ベタ書きの値を使っていた箇所を変更

--- mstest_money_example/money/Dollar.cs
+++ mstest_money_example/money/Dollar.cs
@@ -2,14 +2,16 @@ namespace mstest_money_example.money
 {
     public class Dollar
     {
+        public int Amount;
+        
         public Dollar(int amount)
         {
+            Amount = amount;
         }
-
-        public int Amount { get; set; }
 
         public void Times(int multiplier)
         {
+            Amount *= multiplier;
         }
     }
 }
