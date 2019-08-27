435b268c38af21bdbb6f2f5dba4ac5cfb07b0291
n-ogawa <fy59.kou-729@live.jp>
2019-08-27 22:35:27 +0900

    Francのインスタンスを直接生成していた箇所をファクトリメソッドを利用するよう変更

--- mstest_money_example/money/Franc.cs
+++ mstest_money_example/money/Franc.cs
@@ -10,7 +10,7 @@ namespace mstest_money_example.money
 
         public override Money Times(int multiplier)
         {
-            return new Franc(Amount * multiplier, null);
+            return Money.Franc(Amount * multiplier);
         }
     }
 }
