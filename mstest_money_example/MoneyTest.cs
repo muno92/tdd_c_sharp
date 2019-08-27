b88e9fa865df41d2673278c657a89b06dfbb6a00
n-ogawa <fy59.kou-729@live.jp>
2019-08-27 23:04:13 +0900

    EqualsでクラスではなくCurrencyを比較できてるか検証するためのテストコードを追加

--- mstest_money_example/MoneyTest.cs
+++ mstest_money_example/MoneyTest.cs
@@ -38,5 +38,11 @@ namespace mstest_money_example
             Assert.AreEqual("USD", Money.Dollar(1).Currency);
             Assert.AreEqual("CHF", Money.Franc(1).Currency);
         }
+        
+        [TestMethod]
+        public void TestDifferentClassEquality()
+        {
+            Assert.IsTrue(new Money(10, "CHF").Equals(new Franc(10, "CHF")));
+        }
     }
 }
