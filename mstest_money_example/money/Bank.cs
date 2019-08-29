10cc8a660056b2373d7041b0b0eebdddee07f765
n-ogawa <fy59.kou-729@live.jp>
2019-08-29 23:30:31 +0900

    ReduceをExpressionに引き上げ、重複を削除

--- mstest_money_example/money/Bank.cs
+++ mstest_money_example/money/Bank.cs
@@ -4,12 +4,7 @@ namespace mstest_money_example.money
     {
         public Money Reduce(Expression source, string to)
         {
-            if (source is Money money)
-            {
-                return money.Reduce(to);
-            }
-            Sum sum = (Sum) source;
-            return sum.Reduce(to);
+            return source.Reduce(to);
         }
     }
 }
