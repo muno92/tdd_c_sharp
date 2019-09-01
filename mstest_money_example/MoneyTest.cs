bf7b75378ad2afbfe2b9fe57d83a52fa3b3de520
n-ogawa <fy59.kou-729@live.jp>
2019-09-01 23:30:27 +0900

    通貨換算のテストを追加

--- mstest_money_example/MoneyTest.cs
+++ mstest_money_example/MoneyTest.cs
@@ -68,5 +68,14 @@ namespace mstest_money_example
             Money result = bank.Reduce(Money.Dollar(1), "USD");
             Assert.AreEqual(Money.Dollar(1), result);
         }
+        
+        [TestMethod]
+        public void TestReduceMoneyDifferentCurrency()
+        {
+            Bank bank = new Bank();
+            bank.AddRate("CHF", "USD", 2);
+            Money result = bank.Reduce(Money.Franc(2), "USD");
+            Assert.AreEqual(Money.Dollar(1), result);
+        }
     }
 }
