4c98a21f88beeaabf75280087c8739c31e28351f
n-ogawa <fy59.kou-729@live.jp>
2019-11-18 22:32:12 +0900

    Timesの副作用をチェックするテストを追加（テストは失敗）

--- xunit_money_example/MoneyTest.cs
+++ xunit_money_example/MoneyTest.cs
@@ -11,6 +11,8 @@ namespace xunit_money_example
             Dollar five = new Dollar(5);
             five.Times(2);
             Assert.Equal(10, five.Amount);
+            five.Times(3);
+            Assert.Equal(15, five.Amount);
         }
     }
 }
