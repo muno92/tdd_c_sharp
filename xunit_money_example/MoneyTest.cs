a9371d92b79edc6e2f0cf4068c73ffb840726690
n-ogawa <fy59.kou-729@live.jp>
2019-11-18 22:15:44 +0900

    最初のテストを作成し、TODOを追加

--- xunit_money_example/MoneyTest.cs
+++ xunit_money_example/MoneyTest.cs
@@ -5,8 +5,11 @@ namespace xunit_money_example
     public class MoneyTest
     {
         [Fact]
-        public void Test1()
+        public void TestMultiplication()
         {
+            Dollar five = new Dollar();
+            five.times(2);
+            Assert.Equal(10, five.amount);
         }
     }
 }
