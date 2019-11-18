e8aa3fb7b9e77adbb2b3583c0acac21ab0050cbd
n-ogawa <fy59.kou-729@live.jp>
2019-11-18 22:34:49 +0900

    Timesが新たなDollarを返すよう変更

--- xunit_money_example/MoneyTest.cs
+++ xunit_money_example/MoneyTest.cs
@@ -9,10 +9,10 @@ namespace xunit_money_example
         public void TestMultiplication()
         {
             Dollar five = new Dollar(5);
-            five.Times(2);
-            Assert.Equal(10, five.Amount);
-            five.Times(3);
-            Assert.Equal(15, five.Amount);
+            Dollar product = five.Times(2);
+            Assert.Equal(10, product.Amount);
+            product = five.Times(3);
+            Assert.Equal(15, product.Amount);
         }
     }
 }
