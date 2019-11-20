bf1f1cb00175a58ed0a4d45c3976cd926c85eda2
n-ogawa <fy59.kou-729@live.jp>
2019-11-20 23:49:21 +0900

    FrancのEqualsをMoneyに引き上げるために、Francの等価性を検証するテストを追加

--- xunit_money_example/MoneyTest.cs
+++ xunit_money_example/MoneyTest.cs
@@ -18,6 +18,8 @@ namespace xunit_money_example
         {
             Assert.True(new Dollar(5).Equals(new Dollar(5)));
             Assert.False(new Dollar(5).Equals(new Dollar(6)));
+            Assert.True(new Franc(5).Equals(new Franc(5)));
+            Assert.False(new Franc(5).Equals(new Franc(6)));
         }
 
         [Fact]
