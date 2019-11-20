5bc50d1901c82bda99fc979024d366616280be7b
n-ogawa <fy59.kou-729@live.jp>
2019-11-20 23:51:55 +0900

    FrancからAmount、Equals()を引き上げ

--- xunit_money_example/money/Franc.cs
+++ xunit_money_example/money/Franc.cs
@@ -1,9 +1,7 @@
 namespace xunit_money_example.money
 {
-    public class Franc
+    public class Franc : Money
     {
-        private int Amount;
-
         public Franc(int amount)
         {
             Amount = amount;
@@ -12,12 +10,6 @@ namespace xunit_money_example.money
         public Franc Times(int multiplier)
         {
             return new Franc(Amount * multiplier);
-        }
-
-        public override bool Equals(object obj)
-        {
-            Franc Franc = (Franc) obj;
-            return Franc != null && Amount == Franc.Amount;
         }
     }
 }
